2e53b95a8cb73ab4176a280877876f01f07eeb06
slamdunk7575 <slamdunk7575@gmail.com>
2024-04-11 21:07:37 +0900

    (feat) Post 도메인 모델 정의 (원본 데이터 관리용)

--- adapter/mysql/src/main/resources/schema.sql
+++ adapter/mysql/src/main/resources/schema.sql
@@ -1,12 +1,12 @@
--- content 테이블 생성
-CREATE TABLE content
+-- post 테이블 생성
+CREATE TABLE post
 (
-    id          INT AUTO_INCREMENT PRIMARY KEY COMMENT '콘텐츠의 id',
-    title       VARCHAR(100) NOT NULL COMMENT '콘텐츠의 제목',
-    content     TEXT COMMENT '콘텐츠의 내용',
-    user_id     INT COMMENT '콘텐츠의 작성자 user id',
-    category_id INT COMMENT '콘텐츠의 카테고리 id',
-    created_at  TIMESTAMP DEFAULT CURRENT_TIMESTAMP COMMENT '콘텐츠의 생성일시',
-    updated_at  TIMESTAMP DEFAULT CURRENT_TIMESTAMP COMMENT '콘텐츠의 수정일시',
-    deleted_at  TIMESTAMP COMMENT '콘텐츠 삭제일시(미삭제 시 NULL)'
+    id          INT AUTO_INCREMENT PRIMARY KEY COMMENT '콘텐츠 id',
+    title       VARCHAR(100) NOT NULL COMMENT '콘텐츠 제목',
+    content     TEXT COMMENT '콘텐츠 내용',
+    user_id     INT COMMENT '콘텐츠 작성자 user id',
+    category_id INT COMMENT '콘텐츠 카테고리 id',
+    created_date  TIMESTAMP DEFAULT CURRENT_TIMESTAMP COMMENT '콘텐츠 생성일시',
+    updated_date  TIMESTAMP DEFAULT CURRENT_TIMESTAMP COMMENT '콘텐츠 수정일시',
+    deleted_date  TIMESTAMP COMMENT '콘텐츠 삭제일시(미삭제 시 NULL)'
 );
---
+++ domain/src/main/java/com/yanggang/autoinspection/content/model/Post.java
@@ -0,0 +1,71 @@
+package com.yanggang.autoinspection.content.model;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+// 원본 데이터 관리용
+@NoArgsConstructor
+@Getter
+public class Post {
+
+    private Long id;
+    private String title;
+    private String content;
+    private Long userId;
+    private Long categoryId;
+    private LocalDateTime createdDate;
+    private LocalDateTime updatedDate;
+    private LocalDateTime deletedDate;
+
+    @Builder
+    public Post(Long id, String title, String content, Long userId, Long categoryId,
+                LocalDateTime createdDate, LocalDateTime updatedDate, LocalDateTime deletedDate) {
+        this.id = id;
+        this.title = title;
+        this.content = content;
+        this.userId = userId;
+        this.categoryId = categoryId;
+        this.createdDate = createdDate;
+        this.updatedDate = updatedDate;
+        this.deletedDate = deletedDate;
+    }
+
+    public Post update(String title, String content, Long categoryId) {
+        this.title = title;
+        this.content = content;
+        this.categoryId = categoryId;
+        this.updatedDate = LocalDateTime.now();
+        return this;
+    }
+
+    public Post delete() {
+        LocalDateTime now = LocalDateTime.now();
+        this.updatedDate = now;
+        this.createdDate = now;
+        return this;
+    }
+
+    public Post undelete() {
+        this.deletedDate = null;
+        return this;
+    }
+
+    public static Post of(
+            String title,
+            String content,
+            Long userId,
+            Long categoryId) {
+        LocalDateTime now = LocalDateTime.now();
+        return Post.builder()
+                .title(title)
+                .content(content)
+                .userId(userId)
+                .categoryId(categoryId)
+                .createdDate(now)
+                .updatedDate(now)
+                .build();
+    }
+}
