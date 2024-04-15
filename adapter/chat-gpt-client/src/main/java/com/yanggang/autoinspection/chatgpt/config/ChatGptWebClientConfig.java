07bc922a1b5d51e64794f59ac018db1f7ca619b6
slamdunk7575 <slamdunk7575@gmail.com>
2024-04-16 00:40:02 +0900

    (feat) ChatGPT에서 사용할 WebClient 설정 추가

---
+++ adapter/chat-gpt-client/src/main/java/com/yanggang/autoinspection/chatgpt/config/ChatGptWebClientConfig.java
@@ -0,0 +1,19 @@
+package com.yanggang.autoinspection.chatgpt.config;
+
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@Configuration
+public class ChatGptWebClientConfig {
+
+    @Bean
+    @Qualifier("chatGptWebClient")
+    public WebClient chatGptWebClient() {
+        return WebClient.builder()
+                .baseUrl("https://api.openai.com")
+                .build();
+    }
+
+}
