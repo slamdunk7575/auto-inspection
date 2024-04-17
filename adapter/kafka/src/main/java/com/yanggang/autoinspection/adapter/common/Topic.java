3637bc4b1196365c832a8b3f22e04535a4d70214
slamdunk7575 <slamdunk7575@gmail.com>
2024-04-17 16:29:50 +0900

    (feat) 검수된 콘텐츠 Topic 추가

--- adapter/kafka/src/main/java/com/yanggang/autoinspection/adapter/common/Topic.java
+++ adapter/kafka/src/main/java/com/yanggang/autoinspection/adapter/common/Topic.java
@@ -3,4 +3,5 @@ package com.yanggang.autoinspection.adapter.common;
 public class Topic {
 
     public static final String ORIGINAL_POST_TOPIC = "post.original";
+    public static final String INSPECTED_POST_TOPIC = "post.inspected";
 }
