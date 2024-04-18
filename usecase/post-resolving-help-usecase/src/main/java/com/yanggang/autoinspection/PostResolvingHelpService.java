11b87a1fc60f81c665bf35ab29964f895a47f931
slamdunk7575 <slamdunk7575@gmail.com>
2024-04-18 19:12:35 +0900

    (feat) post-resolving-help-usecase 캐싱 레이어 적용

--- usecase/post-resolving-help-usecase/src/main/java/com/yanggang/autoinspection/PostResolvingHelpService.java
+++ usecase/post-resolving-help-usecase/src/main/java/com/yanggang/autoinspection/PostResolvingHelpService.java
@@ -11,18 +11,32 @@ public class PostResolvingHelpService implements PostResolvingHelpUsecase {
 
     private final PostPort postPort;
     private final MetadataPort metadataPort;
+    private final ResolvedPostCachePort resolvedPostCachePort;
 
-    public PostResolvingHelpService(PostPort postPort, MetadataPort metadataPort) {
+    public PostResolvingHelpService(PostPort postPort,
+                                    MetadataPort metadataPort,
+                                    ResolvedPostCachePort resolvedPostCachePort) {
         this.postPort = postPort;
         this.metadataPort = metadataPort;
+        this.resolvedPostCachePort = resolvedPostCachePort;
     }
 
     @Override
     public ResolvedPost resolvePostById(Long postId) {
+
+        ResolvedPost resolvedPost = resolvedPostCachePort.get(postId);
+        if (resolvedPost != null) {
+            System.out.println("----------- 레디스 캐싱 조회 ----------");
+            return resolvedPost;
+        }
+
+        System.out.println("----------- DB 조회 ----------");
         Post post = postPort.findById(postId);
         String userName = metadataPort.getUserNameByUserId(post.getUserId());
         String categoryName = metadataPort.getCategoryNameByCategoryId(post.getCategoryId());
-        ResolvedPost resolvedPost = ResolvedPost.generate(post, userName, categoryName);
+        resolvedPost = ResolvedPost.generate(post, userName, categoryName);
+        resolvedPostCachePort.set(resolvedPost);
+
         return resolvedPost;
     }
 
