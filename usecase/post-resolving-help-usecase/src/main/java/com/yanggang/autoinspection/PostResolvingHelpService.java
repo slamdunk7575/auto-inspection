cc08ef76b546ac9bb867cd33aceb28ef3d0c1d99
slamdunk7575 <slamdunk7575@gmail.com>
2024-04-18 21:12:41 +0900

    (refactor) Write-Through 방식으로 캐싱 전략 리팩토링

--- usecase/post-resolving-help-usecase/src/main/java/com/yanggang/autoinspection/PostResolvingHelpService.java
+++ usecase/post-resolving-help-usecase/src/main/java/com/yanggang/autoinspection/PostResolvingHelpService.java
@@ -23,19 +23,17 @@ public class PostResolvingHelpService implements PostResolvingHelpUsecase {
 
     @Override
     public ResolvedPost resolvePostById(Long postId) {
-
+        // Cache 조회
         ResolvedPost resolvedPost = resolvedPostCachePort.get(postId);
         if (resolvedPost != null) {
-            System.out.println("----------- 레디스 캐싱 조회 ----------");
             return resolvedPost;
         }
 
-        System.out.println("----------- DB 조회 ----------");
+        // DB 조회
         Post post = postPort.findById(postId);
-        String userName = metadataPort.getUserNameByUserId(post.getUserId());
-        String categoryName = metadataPort.getCategoryNameByCategoryId(post.getCategoryId());
-        resolvedPost = ResolvedPost.generate(post, userName, categoryName);
-        resolvedPostCachePort.set(resolvedPost);
+        if (post != null) {
+            resolvedPost = resolvedPost(post);
+        }
 
         return resolvedPost;
     }
@@ -43,5 +41,30 @@ public class PostResolvingHelpService implements PostResolvingHelpUsecase {
     @Override
     public List<ResolvedPost> resolvePostsByIds(List<Long> postIds) {
         return postIds.stream().map(this::resolvePostById).toList();
+    }
+
+    @Override
+    public void resolvePostAndSave(Post post) {
+        ResolvedPost resolvedPost = resolvedPost(post);
+        if (resolvedPost != null) {
+            resolvedPostCachePort.set(resolvedPost);
+        }
+    }
+
+    @Override
+    public void deleteResolvedPost(Long postId) {
+        resolvedPostCachePort.delete(postId);
+    }
+
+    private ResolvedPost resolvedPost(Post post) {
+        if (post == null) {
+            return null;
+        }
+
+        String userName = metadataPort.getUserNameByUserId(post.getUserId());
+        String categoryName = metadataPort.getCategoryNameByCategoryId(post.getCategoryId());
+        ResolvedPost resolvedPost = ResolvedPost.generate(post, userName, categoryName);
+        resolvedPostCachePort.set(resolvedPost);
+        return resolvedPost;
     }
 }
--- usecase/post-resolving-help-usecase/src/main/java/com/yanggang/autoinspection/PostResolvingHelpUsecase.java
+++ usecase/post-resolving-help-usecase/src/main/java/com/yanggang/autoinspection/PostResolvingHelpUsecase.java
@@ -1,5 +1,6 @@
 package com.yanggang.autoinspection;
 
+import com.yanggang.autoinspection.content.post.model.Post;
 import com.yanggang.autoinspection.content.post.model.ResolvedPost;
 
 import java.util.List;
@@ -9,4 +10,8 @@ public interface PostResolvingHelpUsecase {
     ResolvedPost resolvePostById(Long postId);
 
     List<ResolvedPost> resolvePostsByIds(List<Long> postIds);
+
+    void resolvePostAndSave(Post post);
+
+    void deleteResolvedPost(Long postId);
 }
