fbfc14a9b45e6192feb119a88ce34c2387d2dc8b
slamdunk7575 <slamdunk7575@gmail.com>
2024-04-19 17:52:18 +0900

    (feat) ES에 색인된 컨텐츠를 keyword 검색 가능하도록 Port, Adapter 구현

--- adapter/elasticsearch/src/main/java/com/yanggang/autoinspection/contentsearch/PostSearchAdapter.java
+++ adapter/elasticsearch/src/main/java/com/yanggang/autoinspection/contentsearch/PostSearchAdapter.java
@@ -3,17 +3,28 @@ package com.yanggang.autoinspection.contentsearch;
 import com.yanggang.autoinspection.PostSearchPort;
 import com.yanggang.autoinspection.content.inspectedpost.model.InspectedPost;
 import com.yanggang.autoinspection.content.post.model.Post;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.elasticsearch.core.ElasticsearchOperations;
+import org.springframework.data.elasticsearch.core.SearchHit;
+import org.springframework.data.elasticsearch.core.SearchHits;
+import org.springframework.data.elasticsearch.core.query.Criteria;
+import org.springframework.data.elasticsearch.core.query.CriteriaQuery;
+import org.springframework.data.elasticsearch.core.query.Query;
 import org.springframework.stereotype.Component;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Component
 public class PostSearchAdapter implements PostSearchPort {
 
     private final PostSearchRepository postSearchRepository;
+    private final ElasticsearchOperations elasticsearchOperations;
 
-    public PostSearchAdapter(PostSearchRepository postSearchRepository) {
+    public PostSearchAdapter(PostSearchRepository postSearchRepository,
+                             ElasticsearchOperations elasticsearchOperations) {
         this.postSearchRepository = postSearchRepository;
+        this.elasticsearchOperations = elasticsearchOperations;
     }
 
     @Override
@@ -24,6 +35,33 @@ public class PostSearchAdapter implements PostSearchPort {
     @Override
     public void deletePost(Long id) {
         postSearchRepository.deleteById(id);
+    }
+
+    @Override
+    public List<Long> searchPostIdsByKeyword(String keyword, int pageNumber, int pageSize) {
+        if (keyword == null || keyword.isBlank() || pageNumber < 0 || pageSize < 0) {
+            return List.of();
+        }
+
+        Query query = buildQuery(keyword, pageNumber, pageSize);
+        SearchHits<PostDocument> searchResult = elasticsearchOperations.search(query, PostDocument.class);
+
+        List<Long> searchedPostIds = searchResult.getSearchHits().stream()
+                .map(SearchHit::getContent)
+                .map(PostDocument::getId)
+                .toList();
+
+        return searchedPostIds;
+    }
+
+    private Query buildQuery(String keyword, int pageNumber, int pageSize) {
+        Criteria criteria = new Criteria("title").matches(keyword)
+                .or(new Criteria("content").matches(keyword))
+                .or(new Criteria("categoryName").is(keyword))
+                .or(new Criteria("tags").is(keyword));
+
+        return new CriteriaQuery(criteria)
+                .setPageable(PageRequest.of(pageNumber, pageSize));
     }
 
     public PostDocument toDocument(InspectedPost inspectedPost) {
--- usecase/core/src/main/java/com/yanggang/autoinspection/PostSearchPort.java
+++ usecase/core/src/main/java/com/yanggang/autoinspection/PostSearchPort.java
@@ -2,9 +2,13 @@ package com.yanggang.autoinspection;
 
 import com.yanggang.autoinspection.content.inspectedpost.model.InspectedPost;
 
+import java.util.List;
+
 public interface PostSearchPort {
 
     void indexPost(InspectedPost post);
 
     void deletePost(Long id);
+
+    List<Long> searchPostIdsByKeyword(String keyword, int pageNumber, int pageSize);
 }
